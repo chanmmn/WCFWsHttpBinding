46153d0464596567b153fdb4ee4ff1a5bb5159e0
chanmmn <33271651+chanmmn@users.noreply.github.com>
2020-08-01 10:47:47 +0800

    Uplod WcfServiceWsHttpBinding
    Uplod WcfServiceWsHttpBinding and the client

---
+++ ConAppWCFWsHttpClient/ConAppWCFWsHttpClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppWCFClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.Service1Client service1 = new ServiceReference1.Service1Client();
+            string result = service1.GetData(10);
+            Console.WriteLine(result);
+        }
+    }
+}
